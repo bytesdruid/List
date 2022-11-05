2a62db6028a79ecd1e9e1a97f8bb716f533d21e0
Joshua Bloom <bloomblockchain@gmail.com>
2022-11-05 18:24:01 -0400

    Added opt in button.

--- to-do-list_app/src/App.js
+++ to-do-list_app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {PeraWalletConnect} from '@perawallet/connect';
-import algosdk from 'algosdk';
+import algosdk, { waitForConfirmation } from 'algosdk';
 import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -13,7 +13,7 @@ import Button from '@material-ui/core/Button';
 const peraWallet = new PeraWalletConnect();
 
 // app ID from the testnet deployment
-const appIndex = 120454587;
+const appIndex = 120823072;
 
 // algorand node connection instantiated
 const algod = new algosdk.Algodv2('','https://testnet-api.algonode.cloud', 443);
@@ -42,21 +42,43 @@ function App() {
     <Container>
       <h1 className='top-div'>Critical Task List</h1>
       <div>
-        <div className='centered-div'><Button variant="outlined" color="primary"
-          onClick={isConnectedToPeraWallet ? handleDisconnectWalletClick : handleConnectionWalletClick
-          }>
-          {isConnectedToPeraWallet ? "Disconnect": "Connect to Pera Wallet"}
-        </Button></div>
-      </div>
-      <div>
-        <div className='centered-div'><Button variant="outlined" color="primary"
+        <div className='centered-div'>
+          <Button variant="outlined" color="primary"
             onClick={isConnectedToPeraWallet ? handleDisconnectWalletClick : handleConnectionWalletClick
             }>
             {isConnectedToPeraWallet ? "Disconnect": "Connect to Pera Wallet"}
-          </Button></div>
+          </Button>
+        </div>
+      </div>
+      <div>
+        <div className='centered-div'>
+          <Button variant='outlined' color="primary"
+            onClick={
+              () => optInToApp()
+            }>
+            Opt-in
+          </Button>
+        </div>
       </div>
     </Container>
   );
+
+  // function that opts account into application
+  async function optInToApp() {
+    const suggestedParams = await algod.getTransactionParams().do();
+    const optInTxn = algosdk.makeApplicationOptInTxn(
+      accountAddress,
+      suggestedParams,
+      appIndex
+    );
+
+    const optInTxGroup = [{txn: optInTxn, signers: [accountAddress]}];
+
+      const signedTx = await peraWallet.signTransaction([optInTxGroup]);
+      console.log(signedTx);
+      const { txId } = await algod.sendRawTransaction(signedTx).do();
+      const result = await waitForConfirmation(algod, txId, 2);
+  }
 
   // function that handles the connection to pera wallet through the UI
   function handleConnectionWalletClick() {
